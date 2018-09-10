713abb8951f7f5be09e0ef2d17d2557fe03956d7
Steven Zapata <szapata427@gmail.com>
2018-09-10 20:16:34 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,236 @@
-# Write your code here!
+def game_hash
+  game_hash = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => {
+        "Alan Anderson" => {
+                 number: 0,
+                 shoe: 16,
+                 points: 22,
+                 rebounds: 12,
+                 assists: 12,
+                 steals: 3,
+                 blocks: 1,
+                 slam_dunks: 1
+               },
+"Reggie Evans" => {
+                 number: 30,
+                 shoe: 14,
+                 points: 12,
+                 rebounds: 12,
+                 assists: 12,
+                 steals: 12,
+                 blocks: 12,
+                 slam_dunks: 7
+               },
+               "Brook Lopez" => {
+                 number: 11,
+                 shoe: 17,
+                 points: 17,
+                 rebounds: 19,
+                 assists: 10,
+                 steals: 3,
+                 blocks: 1,
+                 slam_dunks: 15
+               },
+               "Mason Plumlee" => {
+                 number: 1,
+                 shoe: 19,
+                 points: 26,
+                 rebounds: 12,
+                 assists: 6,
+                 steals: 3,
+                 blocks: 8,
+                 slam_dunks: 5
+               },
+               "Jason Terry" => {
+                 number: 31,
+                 shoe: 15,
+                 points: 19,
+                 rebounds: 2,
+                 assists: 2,
+                 steals: 4,
+                 blocks: 11,
+                 slam_dunks: 1
+               }
+    }
+  },
+  away: {
+    team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {
+        "Jeff Adrien" => {
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        },
+        "Bismak Biyombo" => {
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
+        },
+        "DeSagna Diop" => {
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+        },
+        "Ben Gordon" => {
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+        },
+        "Brendan Haywood" => {
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12
+        }
+      }
+    }
+  }
+end
+
+
+
+
+# require "pry"
+# def good_practices
+#   game_hash.each do |location, team_data|
+#     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
+#     binding.pry
+#       team_data.each do |attribute, data|
+#         #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
+#         binding.pry
+ 
+#         #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
+#         data.each do |data_item|
+#             binding.pry
+#       end
+#     end
+#   end
+# end
+
+
+# puts good_practices
 
 
 
 
 
+def num_points_scored(name)
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player, stats|
+      if player == name
+        return stats[:points]
+      end
+  end
+end
+
+end
+
+
+def shoe_size(name)
+    game_hash.each do |team, team_info|
+    team_info[:players].each do |player, stats|
+      if player == name
+        return stats[:shoe]
+      end
+  end
+end
+
+end
+  
+def team_colors(team)
+  game_hash.each do |teams, info|
+    if info[:team_name] == team
+      return (info[:colors].collect do |color|
+         color
+      end)
+    end
+  end
+end
+
+# require "pry"
+def team_names
+  array = []
+  game_hash.each do |teams, names|
+    array << names[:team_name]
+ end
+ return array
+end
+  # binding.pry
+  
+  
+  def player_numbers(name_team)
+    array = []
+    
+    game_hash.each do |team, info|
+      if name_team == info[:team_name]
+        info[:players].each do |name, stats|
+          array << stats[:number]
+        end
+      end
+    end
+    return array 
+  end
+     
+  def player_stats(name)
+
+game_hash.each do |team, info|
+  info[:players].each do |names, stats|
+    if names == name
+    return stats
+  end
+end
+end
+end
 
 
 
+def big_shoe_rebounds
+  
+  biggest_shoe = 0
+  rebound_num = 0
+  
+  game_hash.each do |team, info|
+    info[:players].each do |name, stats|
+      
+   if stats[:shoe] > biggest_shoe
+     
+    biggest_shoe = stats[:shoe]
+    rebound_num = stats[:rebounds]
+    
+    end
+  end
+  end
+   rebound_num
+end
 
 
+  
+  
